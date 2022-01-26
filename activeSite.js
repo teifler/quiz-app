e4d778f4b5d3e12532436c447daf3f5f1f0788ee
Tim Eifler <tim.eif@protonmail.com>
2022-01-26 09:06:42 +0100

    Add javascript to show the active site

---
+++ activeSite.js
@@ -0,0 +1,5 @@
+//Marks the activeSite in the nav
+
+const navBarElement = document.querySelector(".js-activeSite");
+navBarElement.classList.add("app-nav__link--active");
+console.log(navBarElement.classList);
--- bookmark.js
+++
@@ -1,9 +0,0 @@
-const bookmarkElement = document.querySelector("[data-js=bookmark]");
-
-bookmarkElement.addEventListener("click", () => {
-  bookmarkElement.classList.toggle("bg-blue");
-  console.log("its workin");
-});
-
-const navBarElement = document.querySelector("[data-js=bookmarks]");
-navBarElement.classList.add("app-nav__link--active");
