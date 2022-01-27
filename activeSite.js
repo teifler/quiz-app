857df07383465088e7c150a70c353903c7053eca
Tim Eifler <tim.eif@protonmail.com>
2022-01-27 11:32:40 +0100

    Add JS to hide / show answer button

--- activeSite.js
+++ activeSite.js
@@ -2,3 +2,7 @@
 
 const navBarElement = document.querySelector(".js-activeSite");
 navBarElement.classList.add("app-nav__link--active");
+
+const hideShowAnswer = document.querySelector(".app-card__answer");
+
+const hideShowAnser = document.querySelector('[js-data="hide-show"]');
