c440fe9886822cf4a62eebc554faa3df36fbdec3
Tim Eifler <tim.eif@protonmail.com>
2022-01-29 21:07:39 +0100

    fix issue with navbar

--- js/textfieldCounter.js
+++ js/textfieldCounter.js
@@ -1,14 +1,20 @@
 //Limit textfield for create question to 200
 
 export function counterTextField() {
-  const textAreaCounter = document.getElementById('yolo');
-
-  const textFieldCreateElement = document.querySelector(
-    '[data-js="add-question"]'
+  const questionInputElement = document.querySelector(
+    '[data-js="add-question-input"]'
   );
+  const outputElement = document.querySelector('[data-js="outputCounter"]');
 
-  textFieldCreateElement?.addEventListener('input', event => {
-    const result = 200 - textFieldCreateElement.value.length;
-    textAreaCounter.textContent = `Please enter your question:   -- Characters left: ${result}`;
+  questionInputElement?.addEventListener('input', event => {
+    const result = 200 - questionInputElement.value.length;
+    if (result === 0) {
+      outputElement.textContent = `Maximum Characters reached: ${result}`;
+      alert('Maximum of 200 characters reached!');
+    } else if (result <= 1) {
+      outputElement.textContent = `Character remaining: ${result}`;
+    } else {
+      outputElement.textContent = `Characters remaining: ${result}`;
+    }
   });
 }
