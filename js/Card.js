d21a00ebba1c8b58c88d38272dd5744009047f0e
Tim Eifler <tim.eif@protonmail.com>
2022-01-31 09:13:32 +0100

    add js

--- js/Card.js
+++ js/Card.js
@@ -2,9 +2,7 @@ export default function Card(cardElement) {
   const bookmarkSvgElement = cardElement.querySelector(
     '[data-js=bookmark-svg]'
   );
-  console.log(bookmarkSvgElement);
   bookmarkSvgElement.addEventListener('click', () => {
     bookmarkSvgElement.classList.toggle('app-card__bookmark--marked');
-    console.log(bookmarkSvgElement);
   });
 }
